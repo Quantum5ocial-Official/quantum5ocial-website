d8460176295238bbe7cdf047004547059979a211
Quantum5ocial <admin@quantum5ocial.com>
2025-11-27 22:12:06 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,3 +4,27 @@ import type { AppProps } from "next/app";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { supabase } from "../lib/supabaseClient";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    async function checkUser() {
+      const { data } = await supabase.auth.getUser();
+      const user = data?.user;
+
+      if (!user && router.pathname !== "/auth") {
+        router.replace("/auth");
+      }
+    }
+
+    checkUser();
+  }, [router]);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
