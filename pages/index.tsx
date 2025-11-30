a9065a541ba2e50efd76e7ec921833917210cbcd
Quantum5ocial <admin@quantum5ocial.com>
2025-11-30 01:17:04 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -471,6 +471,11 @@ export default function Home() {
           </div>
         </section>
 
+        {/* FOOTER */}
+        <footer>
+          Quantum5ocial © 2025 · Building the quantum ecosystem ·{" "}
+          <a href="mailto:info@quantum5ocial.com">Contact</a>
+        </footer>
       </div>
     </>
   );
