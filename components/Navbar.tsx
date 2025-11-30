10874bfe1ab310a87395113da1e422d6351c75ed
Quantum5ocial <admin@quantum5ocial.com>
2025-11-30 02:26:57 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -88,7 +88,7 @@ export default function Navbar() {
       <Link href="/" className="brand-clickable">
         <div className="brand">
           <img
-            src="/Q5_no_bg.png"
+            src="/Q5_white_bg.png"
             alt="Quantum5ocial logo"
             className="brand-logo"
           />
