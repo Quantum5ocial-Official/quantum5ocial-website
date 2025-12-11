0e2e840ab74d803cd9ec285ed8ac2b8b9f8703ad
Quantum5ocial <admin@quantum5ocial.com>
2025-12-11 15:43:06 +0100

    Update saved-products.tsx

--- pages/dashboard/saved-products.tsx
+++ pages/dashboard/saved-products.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import { supabase } from "../../lib/supabaseClient";
 import { useSupabaseUser } from "../../lib/useSupabaseUser";
 
-const Navbar = dynamic(() => import("../../components/Navbar"), { ssr: false });
+const Navbar = dynamic(() => import("../../components/NavbarIcons"), { ssr: false });
 
 type SavedRow = {
   id: string;
