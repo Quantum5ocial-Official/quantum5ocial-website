d46d2125f51b14bae44a4935625f1e4de8925c66
Quantum5ocial <admin@quantum5ocial.com>
2025-12-11 09:15:18 +0100

    Update index.tsx

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import { supabase } from "../../lib/supabaseClient";
 import { useSupabaseUser } from "../../lib/useSupabaseUser";
 
-const Navbar = dynamic(() => import("../../components/Navbar"), { ssr: false });
+const Navbar = dynamic(() => import("../../components/NavbarIcons"), { ssr: false });
 const LeftSidebar: any = dynamic(
   () => import("../../components/LeftSidebar"),
   { ssr: false }
